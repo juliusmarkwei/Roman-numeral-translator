cb9b0019e65d269a425c3201baa6e9f2bbe4ca60
CEO <79404782+julius-m20@users.noreply.github.com>
2022-07-07 00:33:39 +0000

    Update main.py

--- RomanNumeralsPy/main.py
+++ RomanNumeralsPy/main.py
@@ -4,23 +4,23 @@ corresponding roman numerals and vise versa
 
 Arthur @Julius alias CEO
 '''
-class CEO:
+class Convertor:
     def __init__(self, number):
         self.number = number
         
     def program(self):
-        warehouse= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+        numbers_ = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
         roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
         i = 12 
         roman_numeral = ''
         while self.number != 0:
-            if warehouse[i] <= self.number:
+            if numbers_[i] <= self.number:
                 roman_numeral += roman[i]
-                self.number = self.number - warehouse[i]
+                self.number = self.number - numbers_[i]
             else:
                 i -= 1
         return roman_numeral
     
     
-converter = CEO(5)
-print(converter.program())
+convert = Convertor(5)
+print(convert.program())
