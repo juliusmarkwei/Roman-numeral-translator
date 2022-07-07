01b4542c640892eb80a10af37c0fd7d2a8b46488
CEO <79404782+julius-m20@users.noreply.github.com>
2022-07-07 00:37:56 +0000

    Update main.py

--- RomanNumeralsPy/main.py
+++ RomanNumeralsPy/main.py
@@ -4,19 +4,22 @@ corresponding roman numerals and vise versa
 
 Arthur @Julius alias CEO
 '''
+
 class Convertor:
     def __init__(self, number):
         self.number = number
         
     def program(self):
-        numbers_ = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+        container_of_numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
         roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
-        i = 12 
+        i = 12
+        
+        #result is stored in variable: roman_numeral
         roman_numeral = ''
         while self.number != 0:
-            if numbers_[i] <= self.number:
+            if container_of_numbers[i] <= self.number:
                 roman_numeral += roman[i]
-                self.number = self.number - numbers_[i]
+                self.number = self.number - container_of_numbers[i]
             else:
                 i -= 1
         return roman_numeral
@@ -24,3 +27,4 @@ class Convertor:
     
 convert = Convertor(5)
 print(convert.program())
+
